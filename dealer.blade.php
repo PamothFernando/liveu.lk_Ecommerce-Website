495966e5c8100696747aa50b81fa67ae66f9f22d
W.Pamoth Fernando <fpamothofficial@gmail.com>
2025-09-07 15:38:19 +0530

    Requirements
    Node.Js, Composer, Laravel

---
+++ dealer.blade.php
@@ -0,0 +1,7 @@
+<h2>New Dealer Registration</h2>
+
+<p><strong>Name:</strong> {{ $dealerfullname }}</p>
+<p><strong>Email:</strong> {{ $dealeremail }}</p>
+<p><strong>Phone:</strong> {{ $dealerphone }}</p>
+<p><strong>Company:</strong> {{ $dealercompany }}</p>
+<p><strong>Address:</strong> {{ $dealeraddress }}</p>
